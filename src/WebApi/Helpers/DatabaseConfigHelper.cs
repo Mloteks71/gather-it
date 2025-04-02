91b69f18c900c60e87a4b137b6117a2e0a923334
AmpF5 <lolc44t@gmail.com>
2025-04-02 21:45:06 +0200

    dotnet format

--- src/WebApi/Helpers/DatabaseConfigHelper.cs
+++ src/WebApi/Helpers/DatabaseConfigHelper.cs
@@ -1,14 +1,16 @@
 namespace WebApi.Helpers;
 
-public static class DatabaseConfigHelper {
-    public static string GetPostgresConnectionString(IConfiguration configuration) {
+public static class DatabaseConfigHelper
+{
+    public static string GetPostgresConnectionString(IConfiguration configuration)
+    {
         var dbConfig = configuration.GetSection("Database:PostgreSQL");
-        
+
         var server = dbConfig["Server"];
         var userId = dbConfig["UserId"];
         var password = dbConfig["Password"];
         var database = dbConfig["Database"];
-        
+
         return $"Host={server};Database={database};Username={userId};Password={password};";
     }
 }
