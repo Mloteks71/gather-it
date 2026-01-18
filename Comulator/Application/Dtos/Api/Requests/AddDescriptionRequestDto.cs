fff7fe98b61b6594c72e4a6b0f5f060a2466c60c
Mloteks71 <Mloteks71@gmail.com>
2026-01-18 21:17:05 +0100

    fix

--- Comulator/Application/Dtos/Api/Requests/AddDescriptionRequestDto.cs
+++ Comulator/Application/Dtos/Api/Requests/AddDescriptionRequestDto.cs
@@ -6,19 +6,9 @@ public record AddDescriptionRequestDto
 {
     [Range(1, int.MaxValue, ErrorMessage = "Job ad ID must be greater than zero.")]
     public int Id { get; set; }
-
-    [StringLength(5000, ErrorMessage = "Description cannot exceed 5000 characters.")]
     public string? Description { get; set; }
-
-    [StringLength(5000, ErrorMessage = "Requirements cannot exceed 5000 characters.")]
     public string? Requirements { get; set; }
-
-    [StringLength(5000, ErrorMessage = "Benefits cannot exceed 5000 characters.")]
     public string? Benefits { get; set; }
-
-    [StringLength(500, ErrorMessage = "Workstyle cannot exceed 500 characters.")]
     public string? Workstyle { get; set; }
-
-    [StringLength(5000, ErrorMessage = "AboutProject cannot exceed 5000 characters.")]
     public string? AboutProject { get; set; }
 }
