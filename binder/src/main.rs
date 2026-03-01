00f2bd511403d8f5ca6d6fa5e12fcd0993c959dd
Mateusz Kurzyna <lolc44t@gmail.com>
2026-03-01 16:36:21 +0100

    feat(binder): add amqp connection

--- binder/src/main.rs
+++ binder/src/main.rs
@@ -1,20 +1,45 @@
-use dotenvy::{from_filename, from_filename_override};
-use sqlx::postgres::PgPoolOptions;
+mod config;
+
+use lapin::types::ShortString;
+use sqlx::{Postgres, postgres::PgPoolOptions};
+
+use crate::config::config;
 
 #[tokio::main]
 async fn main() {
-    setup_env();
-
-    let postgres_url = std::env::var("POSTGRES_URL").expect("POSTGRES_URL must be set");
-
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&postgres_url)
-        .await
-        .expect("Failed to connect to the database");
+    let pool = setup_postgres().await;
+    let consumer = setup_rmq().await;
 }
 
-fn setup_env() {
-    let _ = from_filename(".env");
-    let _ = from_filename_override(".env.development");
+async fn setup_postgres() -> sqlx::Pool<Postgres> {
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&config().postgres_url)
+        .await
+        .expect("Failed to connect to the database")
+}
+
+async fn setup_rmq() -> lapin::Consumer {
+    let conn_options = lapin::ConnectionProperties::default();
+
+    let conn = lapin::Connection::connect(&config().rabbitmq_url, conn_options)
+        .await
+        .expect("Failed to connect to RabbitMQ");
+
+    let channel = conn
+        .create_channel()
+        .await
+        .expect("Failed to create channel");
+
+    let consumer_options = lapin::options::BasicConsumeOptions::default();
+
+    channel
+        .basic_consume(
+            ShortString::from(config().rabbitmq_consumer_tag.clone()),
+            ShortString::from(config().rabbitmq_consumer_tag.clone()),
+            consumer_options,
+            Default::default(),
+        )
+        .await
+        .expect("Failed to create consumer")
 }
