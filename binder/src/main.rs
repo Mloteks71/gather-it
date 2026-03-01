43d64b27fa0133f83904e219c5ee674955856bdf
Mateusz Kurzyna <lolc44t@gmail.com>
2026-03-01 12:45:30 +0100

    feat(binder): add postgres connection

--- binder/src/main.rs
+++ binder/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dotenvy::{from_filename, from_filename_override};
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() {
+    setup_env();
+
+    let postgres_url = std::env::var("POSTGRES_URL").expect("POSTGRES_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&postgres_url)
+        .await
+        .expect("Failed to connect to the database");
+}
+
+fn setup_env() {
+    let _ = from_filename(".env");
+    let _ = from_filename_override(".env.development");
 }
