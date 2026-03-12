ab87a4a55782a2e103a8376f5f68a41a1403422b
Mateusz Kurzyna <lolc44t@gmail.com>
2026-03-12 20:29:32 +0100

    feat(binder): add logging

--- binder/src/main.rs
+++ binder/src/main.rs
@@ -4,17 +4,39 @@ mod models;
 mod repositories;
 
 use sqlx::{Postgres, postgres::PgPoolOptions};
+use tracing::info;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 use crate::config::config;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    config();
+    let _guard = setup_tracing();
+    info!("Starting application");
+
     let pool = setup_postgres().await?;
     let consumer = setup_rmq().await?;
 
     bind::bind(&pool, consumer).await?;
 
     Ok(())
+}
+
+fn setup_tracing() -> tracing_appender::non_blocking::WorkerGuard {
+    color_eyre::install().expect("Failed to install color_eyre");
+
+    let file_appender = tracing_appender::rolling::daily("logs", "binder.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        // for stdout logs
+        // .with(fmt::layer().with_writer(std::io::stdout))
+        .with(fmt::layer().with_writer(non_blocking).with_ansi(false))
+        .init();
+
+    _guard
 }
 
 async fn setup_postgres() -> color_eyre::Result<sqlx::Pool<Postgres>> {
