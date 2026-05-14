150aab318be5dca34d9e4ed2b0fa43f15960b99f
Mateusz Kurzyna <lolc44t@gmail.com>
2026-05-14 18:09:16 +0200

    feat(scrapyard, binder): add .env templates file, move connecting to a channel before scraping data

--- binder/src/main.rs
+++ binder/src/main.rs
@@ -32,18 +32,23 @@ async fn main() -> color_eyre::Result<()> {
 fn setup_tracing() -> tracing_appender::non_blocking::WorkerGuard {
     color_eyre::install().expect("Failed to install color_eyre");
 
-    let file_appender = tracing_appender::rolling::daily("logs", "binder.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let (non_blocking, guard) = if config().log_target == "file" {
+        let appender = tracing_appender::rolling::daily("logs", "binder.log");
+        tracing_appender::non_blocking(appender)
+    } else {
+        tracing_appender::non_blocking(std::io::stdout())
+    };
 
     tracing_subscriber::registry()
         .with(EnvFilter::from_default_env())
-        // terminal
-        // .with(fmt::layer().with_writer(std::io::stdout))
-        // file
-        .with(fmt::layer().with_writer(non_blocking).with_ansi(false))
+        .with(
+            fmt::layer()
+                .with_writer(non_blocking)
+                .with_ansi(config().log_target != "file"),
+        )
         .init();
 
-    _guard
+    guard
 }
 
 async fn setup_postgres() -> color_eyre::Result<sqlx::Pool<Postgres>> {
